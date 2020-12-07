d4900eb41f4e772408a2f276c9e4013d7df8a300
Alexey Biliavskyi <oleksiibiliavskyi@gmail.com>
2020-12-07 13:05:23 +0200

    vue deploy files

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# если вы публикуете по адресу https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:AlexeyBiliavskyi/vue-todo-list-upswot.git master:gh-pages
+
+cd -
